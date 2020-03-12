9a5c4ffdd19da5f4ac38095cd799ead6bb4f5b20
Aaron <aarnliebrmn@gmail.com>
2020-03-11 18:11:31 -0700

    more terminal window improvements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Text extends React.Component {
+  constructor(props) {
+    super(props);
+    this.text = props.text;
+    this.color = props.color;
+  }
+  render() {
+    return (
+      <span className={`code ` + `${this.color}`}> {this.text} </span>
+    );
+  }
+}
+
+class Window extends React.Component {
+  render() {
+    return (
+      <div className="window">
+        <div className="window-top d-flex">
+          <div className="dot red ml-3"></div>
+          <div className="dot yellow"></div>
+          <div className="dot green"></div>
+        </div>
+        <div className="window-body d-flex">
+          <ul>
+            <li className="mt-3">
+              <Text color="green" text="aaronlieberman" />
+              <Text color="white" text=":" />
+              <Text color="blue" text="~/Code/web/react/titan_codes" />
+              <Text color="white" text="$ " />
+            </li>
+            <Text color="comment" text="// This is a comment" />
+            <Text color="base" text="Base2" />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -30,16 +69,7 @@ function App() {
         </div>
       </nav>
       <div className="container-fluid">
-        <div className="window">
-          <div className="window-top d-flex">
-            <div className="dot red ml-3"></div>
-            <div className="dot yellow"></div>
-            <div className="dot green"></div>
-          </div>
-          <div className="window-body d-flex">
-            <p className="code ml-3 mt-3"> this is a test </p>
-          </div>
-        </div>
+        <Window />
       </div>
       <footer className="bg-light mt-4">
         <div className="container-fluid py-3">
