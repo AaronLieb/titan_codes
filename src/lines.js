fdeeb6b818fa32c26a02f8603230a96de0365a73
AaronLieb <AaronJLieberman@gmail.com>
2020-04-01 23:43:03 -0700

    fixed white arrow in lines.js

--- src/lines.js
+++ src/lines.js
@@ -1,6 +1,6 @@
 var lines =
 [
-  [{text: "titancodes@csuf", color: "green", time: 50},{text: " ~/code/", color:"blue"},[{text:" >", color:"white"}]],
+  [{text: "titancodes@csuf", color: "green", time: 50},{text: " ~/code/", color:"blue"},{text:" >", color:"white"}],
   [{text:" _______ _ _               _____          _           ", color:"base", time: 50}],
   [{text:"|__   __(_) |             / ____|        | |          ", color:"base", time: 50}],
   [{text:"   | |   _| |_ __ _ _ __ | |     ___   __| | ___  ___ ", color:"base", time: 50}],
