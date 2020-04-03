f35a8b813287062de07886bd0ba167445bb40d36
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-04-03 13:48:26 -0700

    implemented ScheduledSequence

--- src/lineData.js
+++ src/lineData.js
@@ -1,24 +1,31 @@
-var lines =
-[
-  [{text: "titancodes@csuf", color: "green"},
-  {text: " ~/code/", color:"blue"},
-  {text:" >", color:"white"}, {text:"",color:"white",time:800},
-  {text:" python3 title.py", color: "yellow", split: 69}],
-  [{text:" _______ _ _               _____          _           ", color:"white bold", time: 500}],
-  [{text:"|__   __(_) |             / ____|        | |          ", color:"white bold", time: 500}],
-  [{text:"   | |   _| |_ __ _ _ __ | |     ___   __| | ___  ___ ", color:"white bold", time: 1000}],
-  [{text:"   | |  | | __/ _` | '_ \\| |    / _ \\ / _` |/ _ \\/ __|", color:"white bold", time: 1000}],
-  [{text:"   | |  | | || (_| | | | | |___| (_) | (_| |  __/\\__ \\", color:"white bold", time: 50}],
-  [{text:"   |_|  |_|\\__\\__,_|_| |_|\\_____\\___/ \\__,_|\\___||___/", color:"white bold", time: 50}],
-  [{text: "titancodes@csuf", color: "green"},
-  {text: " ~/code/", color:"blue"},
-  {text:" >", color:"white"}, {text:"",color:"white",time:800},
-  {text:" python3 info.py", color: "yellow", split: 69}],
-  [{text:"When: September 19th 2020", color:"white", time: 500}],
-  [{text:"Where: Pollak Library", color:"white"}],
-  [{text: "titancodes@csuf", color: "green", time: 500},
-  {text: " ~/code/", color:"blue"},
-  {text:" >", color:"white"},],
+var lines = [
+  [
+    { details: { text: "titancodes@csuf", color: "green" }, timeout: 0 },
+    { details: { text: " ~/code/", color: "blue" }, timeout: 0 },
+    { details: { text:" >", color: "white" }, timeout: 0 },
+    { details: { text: "", color: "white" }, timeout: 800 },
+    { details: { text: " python3 title.py", color: "yellow" }, split: 69 }
+  ],
+  [{ details: {text:" _______ _ _               _____          _           ", color: "white bold" }, timeout: 500}],
+  [{ details: {text:"|__   __(_) |             / ____|        | |          ", color: "white bold" }, timeout: 500}],
+  [{ details: {text:"   | |   _| |_ __ _ _ __ | |     ___   __| | ___  ___ ", color: "white bold" }, timeout: 1000}],
+  [{ details: {text:"   | |  | | __/ _` | '_ \\| |    / _ \\ / _` |/ _ \\/ __|", color: "white bold" }, timeout: 1000}],
+  [{ details: {text:"   | |  | | || (_| | | | | |___| (_) | (_| |  __/\\__ \\", color: "white bold" }, timeout: 50}],
+  [{ details: {text:"   |_|  |_|\\__\\__,_|_| |_|\\_____\\___/ \\__,_|\\___||___/", color: "white bold" }, timeout: 50}],
+  [
+    { details: {text: "titancodes@csuf", color: "green"}, timeout: 0},
+    { details: {text: " ~/code/", color: "blue" }, timeout: 0 },
+    { details: {text:" >", color: "white"}, timeout: 0 },
+    { details: {text: "", color: "white" }, timeout: 0 },
+    { details: {text: " python3 info.py", color: "yellow" }, split: 69 }
+  ],
+  [{ details: { text: "When: September 19th 2020", color: "white" }, timeout: 500 }],
+  [{ details: { text:"Where: Pollak Library", color: "white" }, timeout: 0 }],
+  [
+    { details: {text: "titancodes@csuf", color: "green"}, timeout: 0},
+    { details: {text: " ~/code/", color: "blue" }, timeout: 0 },
+    { details: {text:" >", color: "white"}, timeout: 0 }
+  ]
 ];
 
 
