abe6af48fc83ad81e2d6637d7710eb1d30c34230
AaronLieb <AaronJLieberman@gmail.com>
2020-04-04 11:35:51 -0700

    moved github logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import TerminalReplica from './components/TerminalReplica.jsx';
 
 
 /* TODO:
-    [] create footer
+    [X] create footer
     [] Move github link to Footer
-    [] add made with <3 in Footer
+    [X] add made with <3 in Footer
     [] max-width: #ch for the text content
     [] scrollTo navbar functionality
     [] Vertically Center TitanCodes in navbar
@@ -65,7 +65,7 @@ function App() {
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
       </div>
 
-      <svg className="wave2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" d="M0,128L48,154.7C96,181,192,235,288,240C384,245,480,203,576,208C672,213,768,267,864,282.7C960,299,1056,277,1152,234.7C1248,192,1344,128,1392,96L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+      <svg className="wave2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" d="M0,256L48,234.7C96,213,192,171,288,154.7C384,139,480,149,576,176C672,203,768,245,864,256C960,267,1056,245,1152,224C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
 
       <div className="container-fluid second">
         <h1> Even More information </h1>
@@ -78,12 +78,22 @@ function App() {
         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
       </div>
 
-      <footer className="bg-light" hidden="true">
+      <footer>
         <div className="container-fluid py-3">
-          <div className="row">
-            <div className="col-1">
-              Footer
-             </div>
+          <div>
+            <a href="https://github.com/AaronLieb/titan_codes">
+              <svg className="icon" width="40" role="img" fill="#eee" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+            </a>
+          </div>
+          <div>
+            Made with ❤️ by &nbsp;
+            <a className="link" href="https://github.com/AaronLieb" target="_blank">
+              @AaronLieb
+            </a>
+            &nbsp; and &nbsp;
+            <a className="link" href="https://github.com/EthanThatOneKid" target="_blank">
+              @EthanThatOneKid
+            </a>
           </div>
         </div>
       </footer>
