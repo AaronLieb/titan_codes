5254f055b10cf2c864e275c4c9901f9d163e00d9
AaronLieb <AaronJLieberman@gmail.com>
2020-04-07 19:25:32 -0700

    better design and timing for faq-questions

--- src/components/FaqQuestion.jsx
+++ src/components/FaqQuestion.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import './FaqQuestion.css';
 
+
 class FaqQuestion extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
+      height: null,
+      styleClosed: {height: 'auto'},
     }
-    // this.targetRef = React.useRef();
   }
+
+  componentDidMount = () => {
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize = () => {
+    this.setState({
+      open: false,
+      styleClosed: {height: 'auto'}});
+    this.setState({
+      height: this.container.offsetHeight,
+      styleClosed: {height: 0}});
+  }
+
   handleClick = e => {
     e.preventDefault();
     this.setState({open: !this.state.open});
   }
+
   render() {
-    // const height = this.targetRef.current.offsetHeight;
-    // console.log(height)
-    let styleOpen = {height: 250 + 'px'};
     return (
       <div className="faq-question">
-        <div className="faq-head" onClick={this.handleClick}> {this.props.question} </div>
-        <div className="faq-body" style={(this.state.open) ? styleOpen : styleClosed }> {this.props.answer} </div>
+        <div className="faq-head" onClick={this.handleClick}>
+          {this.props.question}
+        </div>
+        <div
+          className="faq-body" style={(this.state.open) ? {height:this.state.height+'px'} : this.state.styleClosed} ref={el => (this.container = el)}>
+          {this.props.answer}
+        </div>
       </div>
     );
   }
 }
-
-const styleClosed = {height: 0 + 'px'};
 
 export default FaqQuestion;
